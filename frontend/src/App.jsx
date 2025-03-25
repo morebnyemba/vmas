1322b45499aaad41ca8172b68a7cc353b07e6348
M.Nyemba <morebnyemba@gmail.com>
2025-03-25 02:13:51 +0000

    optimize for choreo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { Header } from "@./components/Header";
-import { Hero } from "@./components/Hero";
-import About from "@./pages/About";
-import Contact from "@./pages/Contact";
-import Services from "@./pages/Services";
+import { Header } from "./components/Header";
+import { Hero } from "./components/Hero";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Services from "./pages/Services";
+import PropertyList from "./pages/PropertyList";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/services" element={<Services />} />
+          <Route path="/property-list" element={<PropertyList />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </div>
