e7ea732891c0fadf9128675c9acdd7bfeb9ec3b0
morebnyemba <morebnyemba@gmail.com>
2025-03-25 06:51:47 +0200

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Hero } from '../components/Hero';
+import { Hero } from '../components/hero';
 import { Card, CardHeader, CardContent, CardFooter, CardTitle, CardDescription } from '../components/ui/Card';
 import { Button } from '../components/ui/Button';
 
