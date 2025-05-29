b96146a2d27a0697b6da5afa41740a6e248d532a
M.Nyemba <morebnyemba@gmail.com>
2025-05-30 01:41:36 +0200

    Latest Update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -182,7 +182,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # This should map to /app/staticfiles in the container
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+os.path.join(BASE_DIR, 'mediafiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
